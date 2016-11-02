cd2f0794e89925bb0fc5acb1da08f3faab37de2d
Redfish <lesonduko@gmail.com>
2016-11-02 17:28:22 +0100

    ajout du test d'upload pour integration

--- ascii.php
+++ ascii.php
@@ -40,5 +40,73 @@ for ($i = 0; $i < $H; $i++)
     
     echo substr($ROW, $pos*$L, $L)."\n";
     //error_log(var_export(($str[12]), true));
-        }
+        }?>
+<?php
+
+	
+
+	fscanf(STDIN, "%d", $L);
+
+	fscanf(STDIN, "%d", $H);
+
+	$T = stream_get_line(STDIN, 256 + 1, "\n");
+
+	$T = str_split(strtoupper($T));
+
+	
+
+	for ($i = 0; $i < $H; $i++) {
+
+		$ROW = stream_get_line(STDIN, 1024 + 1, "\n");
+
+		foreach ($T as $letter) {
+
+			$ascii_code = ord($letter);
+
+			$position = ($ascii_code < 65 || $ascii_code > 90) ? 26 : $ascii_code - 65 ;
+
+			$position *= $L;
+
+			echo substr($ROW, $position, $L);
+
+		}
+
+		echo "\n";
+
+	}
+
+	
+
+?>
+
+
+
+
+
+
+
+<?php
+	/**
+	 * Auto-generated code below aims at helping you parse
+	 * the standard input according to the problem statement.
+	 **/
+
+	fscanf(STDIN, "%d",
+	$n // the number of temperatures to analyse
+	
+
+	);
+	for($i = 0; $i < $n; $i++)
+	$temps = stream_get_line(STDIN, 256 + 1, "\n"); // the n temperatures expressed as integers ranging from -273 to 5526
+	$chaud = str_split($temps);
+	foreach ($n as $temps) {
+		# code...
+	}
+
+
+	// Write an action using echo(). DON'T FORGET THE TRAILING \n
+	// To debug (equivalent to var_dump): error_log(var_export($var, true));
+
+echo("result\n");
+?>
     
--- test.php
+++ test.php
@@ -36,9 +36,64 @@
 
 
     }
-        ?>
+
+ $relatedGroups = "SELECT * FROM t_groupe WHERE gro_name =:groupname AND gro_password =:password;";
+        
+        $query = $this->getDb()->prepare($relatedGroups);
+        $query->execute(array(
+            'groupname' => $data['groupname'],
+            'password' => $data['password']));
+        echo 'le select sql : '.$relatedGroups ; 
+
+        if($relatedGroups = null){
+
+            throw new Exception("login group or password is invalid", 1);
+        } else {
+
+            $login = 'vous etes connecté';
+            echo $login ;
+            session_start($relatedGroups);
+            $_session = $data['groupname'];
+            $_session = $data['password'];
+            var_dump($_session);
+    }
+        
 
 
-    
-    
+
+
+
+  $db = "UPDATE `t_groupe` SET gro_temp_key = $key WHERE gro_name = :groupname";
+                $query = $this->getDb()->prepare($relatedGroups);
+                $query->execute(array(
+                    'groupname' => $data['groupname']));
+
+         }
+
+
+
+         $loginkey = true;
          
+                if($loginkey == true)
+                {
+
+                    $key = rand(100, 999);
+                    var_dump($this->db);
+                    var_dump($key);
+                    //$relatedGroups[0]['gro_id']
+                    $db = "UPDATE `t_groupe` SET gro_temp_key = $key WHERE gro_id = :id";
+                    $this->getDb()->prepare($db);
+                    $this->getDb()->execute(array('id' => $relatedGroups[0]['gro_id']));
+
+                    return $app->json(array(
+                        'record'=> $key ,
+                        'status'=> 'ok' ,
+                        'error'=> $e-> getMessage()), 200);
+             if($toto== false) {
+                 throw new \Exception("invalid group or password!");
+            }
+                }
+
+    
+    
+         ?>
